51a71fad9b20c7cf955b90e00127781e59c8897a
jyothi <joeswaroopmakena@gmail.com>
2023-08-25 12:02:27 +0530

    Rating component added

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import Rating from './Rating'
 const Product = ({product}) => {
   return (
     <Card className='py-3 p-3 rounded'>
@@ -10,10 +11,15 @@ const Product = ({product}) => {
 
     <Card.Body>
     <Link href={`/product/${product._id}`}>
-        <Card.Title>
+        <Card.Title className='product-title'>
+          {/* We are adding the class product-title because lets say the product title is short and it does not span 2 lines, then the card component height becomes shorter as compared to others. So, to overcome this, we add text-overflow: elipsis so that the title only spans over one line, and if it is any longer, it is displayed by ... */}
             <strong>{product.name}</strong>
         </Card.Title>
     </Link>
+
+    <Card.Text as='div'>
+      <Rating value={product.rating} text={`${product.numReviews} Reviews`} />
+    </Card.Text>
 
     <Card.Text as='h3'>
         ${product.price}
