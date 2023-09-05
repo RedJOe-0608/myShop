955317b5e6f40afe8463ae00e618a747b08c150c
jyothi <joeswaroopmakena@gmail.com>
2023-09-06 05:13:40 +0530

    "Bug Fix 1"

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,7 +1,6 @@
 import path from 'path'
 import express from 'express'
 import multer from 'multer'
-import { send } from 'process'
 const router = express.Router()
 
 const storage = multer.diskStorage({
