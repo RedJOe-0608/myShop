0b7c5bb1dd2452ed83e5b1a8eb1c1937818151a8
jyothi <joeswaroopmakena@gmail.com>
2023-09-06 05:35:10 +0530

    "Bug Fix 4"

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -25,7 +25,7 @@ const HomePage = () => {
     <Meta />
     <h1>Latest Products</h1>
     <Row>
-        {data.products.map(product =>(
+        {data?.products?.map(product =>(
             <Col key={product._id} sm={12} md={6} lg={4} xl={3} className='my-3'>
             <Product product={product} />
             </Col>
